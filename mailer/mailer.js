2c0881386f089f177ccb44142f11e6b432864a06
rodarg <rodarg87@gmail.com>
2020-09-16 17:41:27 -0300

    semana 3

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 const sgTransport = require('nodemailer-sendgrid-transport')
 let mailConfig;
-    if  (process.env.NODE_ENV === 'produccion') {
+    if  (process.env.NODE_ENV === 'production') {
     const options = {
         auth: {
             api_key: process.env.SENDGRID_API_KEY
