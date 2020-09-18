b9337cb719187ddf0cdbd37a2103b8aee1420e34
rodarg <rodarg87@gmail.com>
2020-09-18 20:38:57 -0300

    semana 3

--- app.js
+++ app.js
@@ -145,12 +145,11 @@ app.post('/resetPassword', function (req, res) {
 /* 
 'https://www.googleapis.com/auth/plus.login',
 'https://www.googleapis.com/auth/plus.profile.emails.read' */
-app.get('auth/google',
+app.get('/auth/google',
   passport.authenticate('google', { scope: [
-    'https://www.googleapis.com/auth/plus.me',
-  'https://www.googleapis.com/auth/userinfo.email'] }));
+    'profile'] }));
 
-app.get('auth/google/callback', 
+app.get('/auth/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication, redirect home.
