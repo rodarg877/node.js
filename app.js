e6ef9c7e76ccae501946a85585d75db8020dfa7b
rodarg <rodarg87@gmail.com>
2020-09-18 13:03:46 -0300

    google oauth2

--- app.js
+++ app.js
@@ -142,15 +142,16 @@ app.post('/resetPassword', function (req, res) {
     });
   });
 });
+/* 
+'https://www.googleapis.com/auth/plus.login',
+'https://www.googleapis.com/auth/plus.profile.emails.read' */
+app.get('auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: [
-    'https://www.googleapis.com/auth/plus.login',
-    'https://www.googleapis.com/auth/plus.profile.emails.read'
-  ] }));
-app.get('/auth/google/callback', 
+app.get('auth/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
+    // Successful authentication, redirect home.
     res.redirect('/');
   });
 
