9a74be5f1db5bc2683710f87e21013a1f7b5d6b2
rodarg <rodarg87@gmail.com>
2020-09-18 23:41:44 -0300

    google oauth2

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ app.post('/resetPassword', function (req, res) {
 });
 
 app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile','email'] }));
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile','https://www.googleapis.com/auth/userinfo.email	'] }));
 
 app.get('/auth/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
